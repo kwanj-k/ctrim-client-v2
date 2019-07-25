dca091300c1b231b387abe44ce2e8ad5d22e9dcc
kwanj-k <mwangikelvin23@gmail.com>
2019-07-25 12:47:31 +0300

    Add new product form controls

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './services/product.service';
 import { SettingsService } from 'src/app/shared/services/settings.service';
 import { IProduct } from './interfaces';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
@@ -21,10 +22,49 @@ export class ProductsComponent implements OnInit {
   prevUrl: string;
   productsExist = true;
   showAddProdModal = false;
+  proFormSubmitted = false;
+  addProductForm = this.fb.group({
+    productName: new FormControl('',
+      [
+        Validators.required
+      ]
+    ),
+    packagingName: new FormControl('',
+      [
+        Validators.required
+      ]
+    ),
+    availablePackages: new FormControl(0,
+      [
+        Validators.required
+      ]
+    ),
+    packagePrice: new FormControl(0,
+      [
+        Validators.required
+      ]
+    ),
+    numberOfPieces: new FormControl(0,
+      [
+        Validators.required
+      ]
+    ),
+    piecePrice: new FormControl(0,
+      [
+        Validators.required
+      ]
+    ),
+    piecesInPackage: new FormControl(0,
+      [
+        Validators.required
+      ]
+    ),
+  });
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductService,
     private settings: SettingsService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit() {
@@ -72,4 +112,18 @@ export class ProductsComponent implements OnInit {
   addProdModal(): void {
     this.showAddProdModal = !this.showAddProdModal;
   }
-}
+
+  addProduct(): void {
+    this.proFormSubmitted = true;
+    const productData = {
+        name: this.addProductForm.get('productName').value,
+        packaging: this.addProductForm.get('packagingName').value,
+        package_pices: this.addProductForm.get('piecesInPackage').value,
+        number_of_packages: this.addProductForm.get('availablePackages').value,
+        package_price: this.addProductForm.get('packagePrice').value,
+        piece_price: this.addProductForm.get('piecePrice').value,
+        number_of_pieces: this.addProductForm.get('numberOfPieces').value,
+    }
+    console.log(productData)
+  }
+} 
